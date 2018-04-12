245326d37563da93363a82d3c8be3bab0b580acd
fcoussi <felipe.coussi@gmail.com>
2018-04-11 21:13:54 -0300

    Se agrego un panel

---
+++ src/ventanas/PanelBoton.java
@@ -0,0 +1,19 @@
+package ventanas;
+
+import javax.swing.JPanel;
+
+public class PanelBoton extends JPanel{
+	
+	public PanelBoton() {
+		
+		inicializarComponentes();
+	}
+	
+	
+	public void inicializarComponentes() {
+		
+		
+	}
+	
+
+}
